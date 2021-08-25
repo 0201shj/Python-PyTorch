9bef3a6e405f91df9acdca0ed15817e6f6b9183d
0201shj <77259440+0201shj@users.noreply.github.com>
2021-08-25 17:25:22 +0900

    Delete Chapter01 directory

--- Chapter01/Scalar01.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-scalar1 = torch.tensor([1.])
-print(scalar1)
-# tensor([1.])
-
-scalar2 = torch.tensor([3.])
-print(scalar2)
-# tensor([3.])
--- Chapter01/Vector01.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-vector1 = torch.tensor([1., 2., 3.])
-print(vector1)
-#tensor([1., 2., 3.])
-
-vector2 = torch.tensor([4., 5., 6.])
-print(vector2)
-#tensor([4., 5., 6.])
